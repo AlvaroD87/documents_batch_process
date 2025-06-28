c62da6b685f179c4d1762131ecb919860be6b9b9
Alvaro David Medel Espinoza <davidal.mes.87@gmail.com>
2025-06-28 11:53:57 -0400

    initial commit

---
+++ src/main/java/local/billy/evaluation/documents_batch_process/DocumentsBatchProcessApplication.java
@@ -0,0 +1,13 @@
+package local.billy.evaluation.documents_batch_process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentsBatchProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentsBatchProcessApplication.class, args);
+	}
+
+}
